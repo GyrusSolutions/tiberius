5232b9ee165799671347b8a1dda8d7dd89b5cad5
Andrey Voitenkov <av@gyrus.biz>
2024-01-26 19:24:34 +0200

    TVP fixed len type mapping as per 2.2.5.5.5.3

--- src/tds/codec/type_info_tvp.rs
+++ src/tds/codec/type_info_tvp.rs
@@ -41,11 +41,11 @@ impl<'a> Encode<BytesMut> for TypeInfoTvp<'a> {
                 //                     ColName ; Column metadata instance
                 dst.put_u32_le(0_u32);
                 dbg!(col.base).encode(dst)?;
-                put_b_varchar("", dst);
-                // put_b_varchar(col.col_name, dst);
+                put_b_varchar("", dst); // 2.2.5.5.5.1: ColName MUST be a zero-length string in the TVP.
+                                        // put_b_varchar(col.col_name, dst);
             }
         } else {
-            dst.put_u16_le(0xFFFF_u16); // TVP_NULL_TOKEN, server knows the type
+            dst.put_u16_le(0xFFFF_u16); // TVP_NULL_TOKEN, server knows the type (never worked in practice)
         }
 
         dst.put_u8(0_u8); // TVP_END_TOKEN
@@ -104,12 +104,121 @@ impl<'a> TypeInfoTvp<'a> {
 
     pub fn with_metadata(self, metadata: Vec<MetaDataColumn<'a>>) -> TypeInfoTvp<'_> {
         let mut metadata = metadata;
+        // 2.2.5.5.5.3
         for mdc in metadata.iter_mut() {
-            if let TypeInfo::FixedLen(t) = mdc.base.ty {
-                if t == FixedLenType::Int4 {
-                    mdc.base.ty =
-                        TypeInfo::VarLenSized(VarLenContext::new(super::VarLenType::Intn, 4, None));
-                }
+            let ty_replace =
+                match mdc.base.ty {
+                    TypeInfo::FixedLen(ref ty) => {
+                        match ty {
+                            FixedLenType::Int1 => Some(TypeInfo::VarLenSized(VarLenContext::new(
+                                super::VarLenType::Intn,
+                                1,
+                                None,
+                            ))),
+                            FixedLenType::Bit => Some(TypeInfo::VarLenSized(VarLenContext::new(
+                                super::VarLenType::Bitn,
+                                1,
+                                None,
+                            ))),
+                            FixedLenType::Int2 => Some(TypeInfo::VarLenSized(VarLenContext::new(
+                                super::VarLenType::Intn,
+                                2,
+                                None,
+                            ))),
+                            FixedLenType::Int4 => Some(TypeInfo::VarLenSized(VarLenContext::new(
+                                super::VarLenType::Intn,
+                                4,
+                                None,
+                            ))),
+                            FixedLenType::Datetime4 => Some(TypeInfo::VarLenSized(
+                                VarLenContext::new(super::VarLenType::Datetimen, 4, None),
+                            )),
+                            FixedLenType::Float4 => Some(TypeInfo::VarLenSized(
+                                VarLenContext::new(super::VarLenType::Floatn, 4, None),
+                            )),
+                            FixedLenType::Money => Some(TypeInfo::VarLenSized(VarLenContext::new(
+                                super::VarLenType::Money,
+                                8,
+                                None,
+                            ))),
+                            FixedLenType::Datetime => Some(TypeInfo::VarLenSized(
+                                VarLenContext::new(super::VarLenType::Datetimen, 8, None),
+                            )),
+                            FixedLenType::Float8 => Some(TypeInfo::VarLenSized(
+                                VarLenContext::new(super::VarLenType::Floatn, 8, None),
+                            )),
+                            FixedLenType::Money4 => Some(TypeInfo::VarLenSized(
+                                VarLenContext::new(super::VarLenType::Money, 4, None),
+                            )),
+                            FixedLenType::Int8 => Some(TypeInfo::VarLenSized(VarLenContext::new(
+                                super::VarLenType::Intn,
+                                8,
+                                None,
+                            ))),
+                            _ => None,
+                        }
+                    }
+                    // TypeInfo::VarLenSized(ref ctx) => match ctx.r#type() {
+                    //     super::VarLenType::Guid => todo!(),
+                    //     super::VarLenType::Intn => todo!(),
+                    //     super::VarLenType::Bitn => todo!(),
+                    //     super::VarLenType::Decimaln => todo!(),
+                    //     super::VarLenType::Numericn => todo!(),
+                    //     super::VarLenType::Floatn => todo!(),
+                    //     super::VarLenType::Money => todo!(),
+                    //     super::VarLenType::Datetimen => todo!(),
+                    //     super::VarLenType::Daten => todo!(),
+                    //     super::VarLenType::Timen => todo!(),
+                    //     super::VarLenType::Datetime2 => todo!(),
+                    //     super::VarLenType::DatetimeOffsetn => todo!(),
+                    //     super::VarLenType::BigVarBin => todo!(),
+                    //     super::VarLenType::BigVarChar => todo!(),
+                    //     super::VarLenType::BigBinary => todo!(),
+                    //     super::VarLenType::BigChar => todo!(),
+                    //     super::VarLenType::NVarchar => todo!(),
+                    //     super::VarLenType::NChar => todo!(),
+                    //     super::VarLenType::Xml => todo!(),
+                    //     super::VarLenType::Udt => todo!(),
+                    //     super::VarLenType::Text => todo!(),
+                    //     super::VarLenType::Image => todo!(),
+                    //     super::VarLenType::NText => todo!(),
+                    //     super::VarLenType::SSVariant => todo!(),
+                    // },
+                    // TypeInfo::VarLenSizedPrecision {
+                    //     ty,
+                    //     size,
+                    //     precision,
+                    //     scale,
+                    // } => match ty {
+                    //     super::VarLenType::Guid => todo!(),
+                    //     super::VarLenType::Intn => todo!(),
+                    //     super::VarLenType::Bitn => todo!(),
+                    //     super::VarLenType::Decimaln => todo!(),
+                    //     super::VarLenType::Numericn => todo!(),
+                    //     super::VarLenType::Floatn => todo!(),
+                    //     super::VarLenType::Money => todo!(),
+                    //     super::VarLenType::Datetimen => todo!(),
+                    //     super::VarLenType::Daten => todo!(),
+                    //     super::VarLenType::Timen => todo!(),
+                    //     super::VarLenType::Datetime2 => todo!(),
+                    //     super::VarLenType::DatetimeOffsetn => todo!(),
+                    //     super::VarLenType::BigVarBin => todo!(),
+                    //     super::VarLenType::BigVarChar => todo!(),
+                    //     super::VarLenType::BigBinary => todo!(),
+                    //     super::VarLenType::BigChar => todo!(),
+                    //     super::VarLenType::NVarchar => todo!(),
+                    //     super::VarLenType::NChar => todo!(),
+                    //     super::VarLenType::Xml => todo!(),
+                    //     super::VarLenType::Udt => todo!(),
+                    //     super::VarLenType::Text => todo!(),
+                    //     super::VarLenType::Image => todo!(),
+                    //     super::VarLenType::NText => todo!(),
+                    //     super::VarLenType::SSVariant => todo!(),
+                    // },
+                    _ => None,
+                };
+            if let Some(ty) = ty_replace {
+                mdc.base.ty = ty;
             }
         }
         TypeInfoTvp {
